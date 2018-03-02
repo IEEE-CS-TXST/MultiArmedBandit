f38bb64232e85fb7709d05e4fa1d0215d4f4197f
Samuel Teich <steich@gmail.com>
2018-03-01 19:34:13 -0600

    First commit

---
+++ multi_armed_bandit.py
@@ -0,0 +1,23 @@
+import random as r
+
+machines = [(.5, .5),
+        (.7, .9),
+        (.2, .3),
+        (.2, 3),
+        (1.5, 1),
+        (3, 2),
+        (2, .1),
+        (.1, 15),
+        (.4, .3),
+        (.9, 1),
+        (1.2, 1)]
+
+
+def get_num_slot_machines():
+    return len(machines)
+
+def use_machine(machine, bet):
+    machine = machines[machine]
+    return int(bet * r.normalvariate(machine[0], machine[1]))
+
+
